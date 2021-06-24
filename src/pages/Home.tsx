065062bf38ce527ddbe026faf47be20493c6078f
EduardoBeg <carlosbegnini7@gmail.com>
2021-06-23 21:49:02 -0300

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,22 @@
+
 import { useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import { auth, firebase } from '../services/firebase'
-
 import '../styles/auth.scss'
 import { Button } from '../components/Button';
-
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle, } = useAuth()
 
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then(result => {
-            history.push('/rooms/new')
-        })
-        
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        history.push('/rooms/new')
     }
 
     return (
@@ -29,21 +27,21 @@ export function Home() {
                 <p>Tire as duvidas da sua audiencia em tempo real</p>
             </aside>
             <main>
-            <div className='main-content'>
-                <img src={logoImg} alt="Letmeask" />
-                <button onClick={handleCreateRoom} className='create-room'>
-                    <img src={googleIconImg} alt="Logo do Google" />
-                    Crie sua sala com o Google 
-                </button>
-                <div className='separator'>ou entre em uma sala</div>
-                <form>
-                    <input type="text"
-                    placeholder="Digite o codigo da sala" />
-                    <Button type="submit">
-                        Entrar na sala
-                    </Button>
-                </form>
-            </div>
+                <div className='main-content'>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button onClick={handleCreateRoom} className='create-room'>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className='separator'>ou entre em uma sala</div>
+                    <form>
+                        <input type="text"
+                            placeholder="Digite o codigo da sala" />
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
             </main>
         </div>
     )
